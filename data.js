ae66130e2fbad62f7bd3730ad8b78061dad71d54
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-04-24 13:00:52 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,77 @@
+const categories = [
+  {
+    "name": "مرستال",
+    "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745263564/v5hfdr3kmdmewvlfuchn.jpg"
+  },
+  {
+    "name": "المسار المغناطيسي",
+    "image": "https://i.postimg.cc/NMRCyxN1/IMG-7308.jpg"
+  },
+  {
+    "name": "ثريا معدن كلاسيك",
+    "image": "https://i.postimg.cc/HxVfv5sK/IMG-7307.jpg"
+  },
+  {
+    "name": "ثريا ليد موديرن",
+    "image": "https://i.postimg.cc/Qd3DwGhg/IMG-7305.jpg"
+  },
+  {
+    "name": "تريات كرستال",
+    "image": "https://m.media-amazon.com/images/I/71gTDrRvIOL._AC_UF1000,1000_QL80_.jpg"
+  },
+  {
+    "name": "مصابيح ليد",
+    "image": "https://i.postimg.cc/MTwBn7xK/IMG-7208.jpg"
+  },
+  {
+    "name": "إضاءة خارجية",
+    "image": "https://cdn.salla.sa/KjYjdb/0lYORcT8H8NPYnGNp1sQbYZfLT6DSnWdD2TFiPTD.jpg"
+  },
+  {
+    "name": "إضاءة داخلية",
+    "image": "https://cdn.salla.sa/WRneP/JIWbcJ7b4MZuYJMXNnp9ifrb18AmvbTGlDIEglI1.jpg"
+  },
+  {
+    "name": "إضاءة مكتبية",
+    "image": "https://m.media-amazon.com/images/I/61jl27ilxtL.jpg"
+  }
+];
+
+const products = [
+  {
+    "category": "تريات كرستال",
+    "name": "ثريا كرستال",
+    "price": 1700,
+    "discountedPrice": 2200,
+    "image": "https://wuilt-assets-v2-dev.s3.amazonaws.com/cl2777uly0044396zj2enh2r6_4fcbd7b6-cc4d-4b5c-b255-193930e460b0.jpg"
+  },
+  {
+    "category": "تريات كرستال",
+    "name": "ثريا كرستال طويل",
+    "price": 2800,
+    "image": "https://m.media-amazon.com/images/I/71nrcOr+VpL._AC_SL1500_.jpg"
+  },
+  {
+    "category": "مصابيح ليد",
+    "name": "مصباح ليد 10 واط",
+    "price": 70,
+    "discountedPrice": 140,
+    "image": "https://i.postimg.cc/MTwBn7xK/IMG-7208.jpg"
+  },
+  {
+    "category": "إضاءة خارجية",
+    "name": "مصباح حديقة مقاوم للماء",
+    "price": 44.5,
+    "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745307709/sif2kddhsab2goktrmdp.jpg",
+    "discountedPrice": 50
+  },
+  {
+    "category": "إضاءة مكتبية",
+    "name": "مصباح مكتب LED",
+    "price": 45.75,
+    "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745269296/mtszbxef5jbt9k7aq1av.jpg"
+  }
+];
+
+window.categories = categories;
+window.products = products;
