79a78b578a6f84f6545cda852c78b5e83a965b6d
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-05-15 20:49:58 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,46 @@
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open("thermal-printer").then(function(cache) {
-      return cache.addAll([
-        "index.html",
-        "app.js",
-        "manifest.json"
-      ]);
-    })
+const CACHE_NAME = 'thermal-printer-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
