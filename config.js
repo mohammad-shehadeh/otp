048205ba6196dc9b2cedaacb87761386db030fb9
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-05-15 21:57:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     TOKEN_PARTS: {
-        PART1: "‏ghp_QbaD0IOUwkEduw6",
-        PART2: "‏o7oQTghtOuHYJKA11hI64"
+        PART1: "‏ghp_C3Ow2K6KRxrPzU5EYKP",
+        PART2: "‏ws6wsIF0tPq1BzSj1"
     },
     REPO: {
         OWNER: "mohammad-shehadeh",
