19f33b27352a82f5ae94788b346ef74132f5a493
mohammad-shehadeh <mshmohamad@iCloud.com>
2025-08-24 14:03:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,9 @@
+
+// تكوين التطبيق
 const CONFIG = {
-    TOKEN_PARTS: {
-        PART1: "‏ghp_eiG7dzoWlGnpAXlQxLAM",
-        PART2: "‏zDyU7yRw073FQlnR"
-    },
     REPO: {
         OWNER: "mohammad-shehadeh",
         NAME: "otp"
     },
-    FILE_PATH: "Server.md"
+    FILE_PATH: "devices.json"
 };
-
-function assembleGitHubToken() {
-    return CONFIG.TOKEN_PARTS.PART1 + CONFIG.TOKEN_PARTS.PART2;
-}
