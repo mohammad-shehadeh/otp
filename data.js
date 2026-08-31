5a6b58838556185c92eb80df39eacd43b89d188e
mohammad-shehadeh <mshmohamad@iCloud.com>
2026-08-31 10:54:30 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,72 +1,67 @@
 const categories = [
   {
-    "name": "مرستال",
-    "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745263564/v5hfdr3kmdmewvlfuchn.jpg"
+    "name": "USB drive",
+    "image": "https://cdn.phototourl.com/free/2026-08-28-71423df7-3e75-41b1-a1e7-532e0477b0d9.jpg"
   },
   {
-    "name": "المسار المغناطيسي",
-    "image": "https://i.postimg.cc/NMRCyxN1/IMG-7308.jpg"
+    "name": "power bank",
+    "image": "https://cdn.phototourl.com/free/2026-08-28-f0c9cc4c-2909-484a-9fbf-cbf590a01dbc.jpg"
   },
   {
-    "name": "ثريا معدن كلاسيك",
-    "image": "https://i.postimg.cc/HxVfv5sK/IMG-7307.jpg"
+    "name": "Airpods",
+    "image": "https://i.postimg.cc/RFxhsrCK/IMG-2012.webp"
   },
   {
-    "name": "ثريا ليد موديرن",
-    "image": "https://i.postimg.cc/Qd3DwGhg/IMG-7305.jpg"
+    "name": "Cable",
+    "image": "https://i.postimg.cc/fTRLgfsB/IMG-2013.webp"
   },
   {
-    "name": "تريات كرستال",
-    "image": "https://m.media-amazon.com/images/I/71gTDrRvIOL._AC_UF1000,1000_QL80_.jpg"
+    "name": "Holder",
+    "image": "https://i.postimg.cc/C5jXbxw7/IMG-2035.jpg"
   },
   {
-    "name": "مصابيح ليد",
-    "image": "https://i.postimg.cc/MTwBn7xK/IMG-7208.jpg"
-  },
-  {
-    "name": "إضاءة خارجية",
-    "image": "https://cdn.salla.sa/KjYjdb/0lYORcT8H8NPYnGNp1sQbYZfLT6DSnWdD2TFiPTD.jpg"
-  },
-  {
-    "name": "إضاءة داخلية",
-    "image": "https://cdn.salla.sa/WRneP/JIWbcJ7b4MZuYJMXNnp9ifrb18AmvbTGlDIEglI1.jpg"
-  },
-  {
-    "name": "إضاءة مكتبية",
-    "image": "https://m.media-amazon.com/images/I/61jl27ilxtL.jpg"
+    "name": "abc",
+    "image": "gggggg"
   }
 ];
 
 const products = [
   {
-    "category": "تريات كرستال",
+    "category": "كرستال",
+    "name": "سايد لامب كريستال P6022-6",
+    "price": 1300,
+    "discountedPrice": null,
+    "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745651866/sb6iipofsj3tqrv661vh.jpg"
+  },
+  {
+    "category": "power bank",
     "name": "ثريا كرستال",
-    "price": 1700,
+    "price": null,
     "discountedPrice": 2200,
     "image": "https://wuilt-assets-v2-dev.s3.amazonaws.com/cl2777uly0044396zj2enh2r6_4fcbd7b6-cc4d-4b5c-b255-193930e460b0.jpg"
   },
   {
-    "category": "تريات كرستال",
+    "category": "power bank",
     "name": "ثريا كرستال طويل",
     "price": 2800,
     "image": "https://m.media-amazon.com/images/I/71nrcOr+VpL._AC_SL1500_.jpg"
   },
   {
-    "category": "مصابيح ليد",
+    "category": "power bank",
     "name": "مصباح ليد 10 واط",
     "price": 70,
     "discountedPrice": 140,
     "image": "https://i.postimg.cc/MTwBn7xK/IMG-7208.jpg"
   },
   {
-    "category": "إضاءة خارجية",
+    "category": "power bank",
     "name": "مصباح حديقة مقاوم للماء",
     "price": 44.5,
     "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745307709/sif2kddhsab2goktrmdp.jpg",
     "discountedPrice": 50
   },
   {
-    "category": "إضاءة مكتبية",
+    "category": "Airpods",
     "name": "مصباح مكتب LED",
     "price": 45.75,
     "image": "https://res.cloudinary.com/dygc5b8cn/image/upload/v1745269296/mtszbxef5jbt9k7aq1av.jpg"
